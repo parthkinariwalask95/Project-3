9972da6778f58376511f2d863fa7168f29e05f26
Andrew <andrewmd4774@gmail.com>
2018-10-10 22:47:25 -0400

    forgot to save

--- project-3/src/pages/signup/signup.js
+++ project-3/src/pages/signup/signup.js
@@ -2,3 +2,7 @@ import React, { Component } from "react";
 import { Formbtn, TextArea, Input } from "../../components/Form";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
+
+class SignUp extends Component {
+
+}
